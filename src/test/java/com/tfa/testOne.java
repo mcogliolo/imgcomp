ea701cd97416175cb353f1056f6a43a4873ea300
Agustin Mascheroni <agustin.mascheroni@cision.com>
2016-04-25 22:48:17 -0300

    Arreglando problemas con la version de JUnit

--- src/test/java/com/tfa/testOne.java
+++ src/test/java/com/tfa/testOne.java
@@ -1,14 +1,15 @@
 package com.tfa;
 
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class testOne {
+	
+	@Test
 	public void verificarTitulo(){
 		WebDriver browser = new FirefoxDriver();
-		
 		browser.get("www.google.com.ar");
-		
 	}
 	
 }
