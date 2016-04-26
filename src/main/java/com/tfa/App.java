770a809ffc1cb6749c61fe6b464c0e7b0b1c9543
Agustin Mascheroni <agustin.mascheroni@cision.com>
2016-04-25 22:42:11 -0300

    Adding project

---
+++ src/main/java/com/tfa/App.java
@@ -0,0 +1,13 @@
+package com.tfa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/tfa/testOne.java
@@ -0,0 +1,14 @@
+package com.tfa;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class testOne {
+	public void verificarTitulo(){
+		WebDriver browser = new FirefoxDriver();
+		
+		browser.get("www.google.com.ar");
+		
+	}
+	
+}
