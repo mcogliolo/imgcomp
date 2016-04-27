071b68fc547beb988aec9903e34b9d803382662f
Mariela Cogliolo <mariela.cogliolo@gmail.com>
2016-04-26 22:32:45 -0300

    Se agrega codigo que permite ingresar y buscar pagina en el navegador

--- src/test/java/com/tfa/testOne.java
+++ src/test/java/com/tfa/testOne.java
@@ -1,15 +1,33 @@
 package com.tfa;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class testOne {
-	
+
 	@Test
-	public void verificarTitulo(){
+	public void verificarTitulo() throws InterruptedException{ {
 		WebDriver browser = new FirefoxDriver();
-		browser.get("www.google.com.ar");
+		browser.get("https://www.google.com.ar");
+
+		WebElement caja = browser.findElement(By.id("lst-ib"));
+
+		caja.sendKeys("Calafate- Argentina");
+		caja.sendKeys(Keys.ENTER);
+
+		Thread.sleep(2000);
+		String resultadoAct = browser.getTitle();
+		String resultadoEsp = "Promociones, Turismo, Hoteles, Excursiones";
+
+		browser.close();
+
+		Assert.assertEquals(resultadoEsp, resultadoAct);
 	}
-	
+
+}
 }
