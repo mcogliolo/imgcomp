6f07863bf83ed8c0c972e311abacb185ef109c0b
Mariela Cogliolo <mariela.cogliolo@gmail.com>
2016-04-28 23:02:46 -0300

    Captura imagenes

--- src/test/java/com/tfa/testOne.java
+++ src/test/java/com/tfa/testOne.java
@@ -1,7 +1,15 @@
 package com.tfa;
 
+import java.awt.AWTException;
+import java.awt.HeadlessException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
@@ -12,44 +20,93 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class testOne {
 
 	@Test
-	public void verificarTitulo() throws InterruptedException, IOException{ {
-		WebDriver browser = new FirefoxDriver();
-		browser.get("https://www.google.com.ar");
+	public void verificarTitulo() throws InterruptedException, IOException, HeadlessException, AWTException {
+		{
+			WebDriver browser = new FirefoxDriver();
+			
+			browser.manage().window().maximize();
+			browser.get("https://www.google.com.ar");
 
-		WebElement caja = browser.findElement(By.id("lst-ib"));
+			WebElement caja = browser.findElement(By.id("lst-ib"));
 
-		String query = "Calafate - Argentina";
-		caja.sendKeys(query);
-		caja.sendKeys(Keys.ENTER);
+			String query = "Calafate - Argentina";
+			caja.sendKeys(query);
+			caja.sendKeys(Keys.ENTER);
 
-		Thread.sleep(2000);
+			Thread.sleep(2000);
 
-		//Variable con el TITULO DEL LINK
-		//HACES EL CODIGO PARA OBTENER EL LINK
-		//LE HACES CLICK AL ELEMENTO LINK
+			// Variable con el TITULO DEL LINK
+			// HACES EL CODIGO PARA OBTENER EL LINK
+			// LE HACES CLICK AL ELEMENTO LINK
 
-		String tituloImagenes = "Imágenes de Calafate - Argentina";
-		WebElement linkImagenes = browser.findElement(By.xpath(".//a[text()='"+tituloImagenes+"']"));
-		linkImagenes.click();
+			String tituloImagenes = "El Calafate - Wikipedia, la enciclopedia libre";
+			WebElement linkImagenes = browser.findElement(By.xpath(".//a[text()='" + tituloImagenes + "']"));
+			linkImagenes.click();
 
-		Thread.sleep(2000);
-		
-		File scrFile = ((TakesScreenshot) browser).getScreenshotAs(OutputType.FILE);
+			Thread.sleep(7000);
 
-		FileUtils.copyFile(scrFile, new File("c:\\tmp\\screenshotff.png"));
+			BufferedImage image = new Robot()
+					.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+			ImageIO.write(image, "png", new File("c:\\tmp\\screenshotff.png"));
+			
+//			File scrFile = ((TakesScreenshot) browser).getScreenshotAs(OutputType.FILE);
+//			FileUtils.copyFile(scrFile, new File("c:\\tmp\\screenshotff.png"));
 
-		String resultadoAct = browser.getTitle();
+			String resultadoAct = browser.getTitle();
 
-		browser.close();
+			browser.close();
 
-		if (!resultadoAct.contains(query))
-			Assert.fail("The query is not in the title");
+			if (!resultadoAct.contains("Wikipedia"))
+				Assert.fail("The query is not in the title");
+		}
+
 	}
 
-}
+	@Test
+	public void verificarTituloChrome() throws InterruptedException, IOException, HeadlessException, AWTException {
+		{
+			System.setProperty("webdriver.chrome.driver", "C:\\eclipse\\chromedriver.exe");
+			WebDriver browser = new ChromeDriver();
+			browser.manage().window().maximize();
+			browser.get("https://www.google.com.ar");
+
+			WebElement caja = browser.findElement(By.id("lst-ib"));
+
+			String query = "Calafate - Argentina";
+			caja.sendKeys(query);
+			caja.sendKeys(Keys.ENTER);
+
+			Thread.sleep(2000);
+
+			// Variable con el TITULO DEL LINK
+			// HACES EL CODIGO PARA OBTENER EL LINK
+			// LE HACES CLICK AL ELEMENTO LINK
+
+			String tituloImagenes = "El Calafate - Wikipedia, la enciclopedia libre";
+			WebElement linkImagenes = browser.findElement(By.xpath(".//a[text()='" + tituloImagenes + "']"));
+			linkImagenes.click();
+
+			Thread.sleep(7000);
+
+			BufferedImage image = new Robot()
+					.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+			ImageIO.write(image, "png", new File("c:\\tmp\\screenshotch.png"));
+//			File scrFile = ((TakesScreenshot) browser).getScreenshotAs(OutputType.FILE);
+//			FileUtils.copyFile(scrFile, new File("c:\\tmp\\screenshotch.png"));
+
+			String resultadoAct = browser.getTitle();
+
+			browser.close();
+
+			if (!resultadoAct.contains("Wikipedia"))
+				Assert.fail("The query is not in the title");
+		}
+
+	}
 }
